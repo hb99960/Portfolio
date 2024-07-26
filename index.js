f1f5c87628fe7fc1306ca7b3b6ff412d80c0d247
Harshit Batra <hb99960@gmail.com>
2024-07-26 13:01:32 +0530

    modal box added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     var readMoreLinks = document.querySelectorAll('.read-more');
 
@@ -13,4 +14,43 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+
+    var projects = document.querySelectorAll(".project-card");
+    var span = document.getElementById("close");
+
+    var modal = document.getElementById("modal-id");
+    var modalImage = document.getElementById("modal-image");
+    var modalText = document.getElementById("modal-text");
+    var modalPeriod = document.getElementById("modal-period");
+    var modalDetail = document.getElementById("modal-detail");
+    // var modalGithub = document.getElementById("modal-github");
+    // var modalLiveDemo = document.getElementById("modal-liveDemo");
+
+   
+    projects.forEach(function(p) {
+        p.addEventListener('click', function(){
+            // alert('Button Clicked!');
+            var image = p.querySelector('.project-image').getAttribute("src")
+            var text = p.querySelector('.project-text').textContent;
+            var period = p.querySelector('.project-period').textContent;
+            var detail = p.querySelector('.project-detail').textContent;
+                
+
+            modal.style.display = "block";
+            modalImage.src = image;
+            modalText.textContent = text;
+            modalPeriod.textContent = period;
+            modalDetail.textContent = detail;
+        });
+    })
+
+    span.onclick = function(){
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
